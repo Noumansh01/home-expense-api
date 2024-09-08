b281a2d6db68feefc75cae3b2f197f214d7a4879
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-09-08 20:49:04 +0500

    user auth

---
+++ controller/Auth/userAuth.js
@@ -0,0 +1,11 @@
+
+
+const userLogin = (req,res)=>{
+res.send("hello i am login");
+};
+
+const userRegister = (req , res)=>{
+    res.send("hello i am register");
+    };
+
+    module.exports={userLogin , userRegister};
