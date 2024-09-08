cd33884f5a667162631b45165967ad5c98c977b2
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-09-08 20:49:17 +0500

    make user route

---
+++ routes/auth/userRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const Router = express.Router();
+const {userLogin}= require("../../controller/Auth/userAuth");
+const {userRegister}= require("../../controller/Auth/userAuth");
+
+
+
+Router.post("/login", userLogin);
+Router.post("/register",userRegister);
