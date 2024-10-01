2289628c58814ff5efe31eacf01d147dd3ba9ace
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-10-01 19:56:18 +0500

    set for body data

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express";
 const app = express();
+import 'dotenv/config';
 const port = process.env.PORT || 4000;
 import Router from "./src/routes/auth/userRoute.js";
 
+// middleware to parse JSON bodies
+app.use(express.json());
+
+// middleware to parse URL-encoded bodies (e.g., from forms)
+app.use(express.urlencoded({ extended: true }));
+
+// middleware to parse route to routes folder 
 app.use("/api/user",Router );
 
 
