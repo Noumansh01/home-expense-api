c4b583db2c5595140b7a48a1cd913022521d6527
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-10-01 18:47:54 +0500

    add dependencies

--- src/controller/Auth/userAuth.js
+++ src/controller/Auth/userAuth.js
@@ -1,11 +1,13 @@
 
 
+export const userRegister = (req , res)=>{
+    res.send("hello i am register");
+    };
+    
 export const userLogin = (req , res)=>{
 res.send("hello i am login");
 };
 
-export const userRegister = (req , res)=>{
-    res.send("hello i am register");
-    };
+
 
     
