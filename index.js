633fd080379d5bb312692ee7417b381a4d159e93
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-10-08 20:18:26 +0500

    call db func in index

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
 import 'dotenv/config';
-const port = process.env.PORT || 4000;
 import userRouter from "./src/routes/auth/userRoute.js";
 import itemRouter from "./src/routes/items/itemsRoute.js";
 import shoppingRouter from "./src/routes/shoppingList/shoppingListRoute.js";
+import {connect_db} from "./src/db/connection.js";
+
+
+const port = process.env.PORT || 4000;
 
 // middleware to parse JSON bodies
 app.use(express.json());
@@ -21,6 +24,8 @@ app.use("/api/shopping",shoppingRouter );
 app.get("/", (req , res)=>{
     res.send("hello world");
 });
+// mongodb connection function calling 
+connect_db();
 
 app.listen(port,()=>{
     console.log(`Running on port ${port}`);
