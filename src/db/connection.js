385cd67072954f18da4cce1845df4017d83be5da
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-10-09 13:01:39 +0500

    miner change

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import 'dotenv/config';
 
 export  const connect_db = ()=>{
-    let db_password= process.env.PASSWORD-DB;
+    let db_password= process.env.PASSWORD_DB;
 
     const db_url=`mongodb+srv://noumansh700:${db_password}@home-expense-db.ekhq5.mongodb.net/?retryWrites=true&w=majority&appName=home-expense-db`;
 mongoose.connect(db_url);
