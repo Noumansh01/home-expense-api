fdfc0f703799090f8f0a0c4c6c233ad393e12ec4
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-10-17 16:20:53 +0500

    making url for database

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,8 +3,9 @@ import 'dotenv/config';
 
 export  const connect_db = ()=>{
     let db_password= process.env.PASSWORD_DB;
+    let db_name= process.env.DB_NAME;
 
-    const db_url=`mongodb+srv://noumansh700:${db_password}@home-expense-db.ekhq5.mongodb.net/?retryWrites=true&w=majority&appName=home-expense-db`;
+    const db_url=`mongodb+srv://noumansh700:${db_password}@home-expense-db.ekhq5.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=home-expense-db`;
 mongoose.connect(db_url);
 console.log("database connected");
 };
